724dcbbd02df5bd3ffbeee8e204ec0d85035fae8
huszad <39922466+huszad@users.noreply.github.com>
2018-07-10 18:16:49 +0200

    Update AI_sudoku.R

--- AI_sudoku.R
+++ AI_sudoku.R
@@ -12,33 +12,39 @@ extreme3 = "   4 28  ,7    9  1,    8  73, 2   79  ,    2    ,  31   6 ,84  1   
 beolvas<-function(sudoku){
 msus<<-matrix(0,9,9)
 msudoku<<-matrix(0,9,9)
-colnames(msudoku)<<-c(1:9)
-rownames(msudoku)<<-c(1:9)
+colnames(msudoku)<<-paste("C",c(1:9),sep = "")
+rownames(msudoku)<<-paste("R",c(1:9),sep = "")
 for(i in 1:9){
-msudoku[i,]<<-rbind(as.numeric(unlist(strsplit(unlist(strsplit(sudoku, ","))[i],NULL))[1:9]))}                                                 
+msudoku[i,]<<-rbind(as.numeric(unlist(strsplit(unlist(strsplit(easy, ","))[i],NULL))[1:9]))}                                                 
 msudoku[is.na(msudoku)]<<-0
-print(msudoku)}
+#print(msudoku)
+}
 
 
 megold<-function(msudoku){
+  m<-Sys.time()
   mstart<<-msudoku
 while(sum(msudoku-msus)){
   psol<-array(0,c(3,9,81)) # lehetséges értékek mátrixa
 for(i in 1:9){for (j in 1:9){
   psol[1,j,which(msudoku[i,]==0)*9-9+i]<-sum(!sum(msudoku[i,]==j)) # lehetséges értékek sor szerint
   psol[2,j,which(msudoku[,i]==0)+(i-1)*9]<-sum(!sum(msudoku[,i]==j)) # lehetséges értékek oszlop szerint
-  if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek négyzetek szerint
+  if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek kis négyzetek szerint
   psol[3,k,(j-1)*9+i]<-sum(!sum(msudoku[(1+3*(ceiling(i/3)-1)):(3+3*(ceiling(i/3)-1)),(1+3*(ceiling(j/3)-1)):(3+3*(ceiling(j/3)-1))]==k))
-  }}}}
+  }}}
+  ms<<-msudoku}
 
 #elmentem új névvel, hogy össze tudjam hasonlítani
   msus<-msudoku
+  
+  
 #beírja a számot, ha csak 1 lehetséges szám van, ami mind a három szűrő szerint szerepelhet
 for(i in 1:81){
   if(sum(colSums(psol[,,i])==3)==1){
     msudoku[i]<-col(psol[,,80])[1,colSums(psol[,,i])==3]
   }}}
-  print(msudoku)}
+  print(msudoku)
+  print(Sys.time()-m)}
 
 
 #----------
@@ -51,9 +57,90 @@ megold(msudoku)
 
 #megnézni, hogy milyen új értékeket írt be az eredetihez képest
 megold(msudoku)-msudoku
- 
 
 
+
+jps<-t(colSums(psol)==3)
+
+for(i in 1:9){for (j in 1:9){
+  if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek kis négyzetek szerint
+    psol[3,k,(j-1)*9+i]<-sum(!sum(msudoku[(1+3*(ceiling(i/3)-1)):(3+3*(ceiling(i/3)-1)),(1+3*(ceiling(j/3)-1)):(3+3*(ceiling(j/3)-1))]==k))
+  }}}}
+
+#genereál sok véletlen mátrixot 
+
+beolvas(easy)
+n<-10*sum(msudoku==0)
+fit<-rep(0,n)
+gen<-array(0,c(9,9,n))
+for(k in 1:n){beolvas(easy)
 for(i in 1:3){for (j in 1:3){
-  assign(paste("m",i,j,sep = ""),msudoku[(1+3*(i-1)):(3+3*(i-1)),(1+3*(j-1)):(3+3*(j-1))])
+  m<-msudoku[(1+3*(i-1)):(3+3*(i-1)),(1+3*(j-1)):(3+3*(j-1))]
+  if(sum(m==0)>0){a<-min(elem[(1+3*(i-1)):(3+3*(i-1)),(1+3*(j-1)):(3+3*(j-1))][elem[(1+3*(i-1)):(3+3*(i-1)),(1+3*(j-1)):(3+3*(j-1))] %in% which(msudoku==0)])
+  m[m==0]<-sample(which(psol[3,,a]==1))
+  msudoku[(1+3*(i-1)):(3+3*(i-1)),(1+3*(j-1)):(3+3*(j-1))]<-m
+  }
+}
+} 
+  gen[,,k]<-msudoku
+  ut<-rep(0,81)
+  for(i in 1:81){
+    ut[i]<-sum(msudoku[row(msudoku)[i],]==msudoku[i])+sum(msudoku[,col(msudoku)[i]]==msudoku[i])-1
+  }
+  fit[k]<-sum(ut)
+}
+
+# a legjobb mátrixnak visszaadja a a cellánkénti előfordulási értékeket
+ez<-gen[,,which(fit==min(fit))[1]]
+for(i in 1:81){
+  ut[i]<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
+}
+
+#véletlen feltöltéshez kellett
+elem<-matrix(1:81,9,9)
+elem[(1+3*(i-1)):(3+3*(i-1)),(1+3*(j-1)):(3+3*(j-1))]
+
+sample(which(psol[3,,6]==1))
+m21[m21==0]<-sample(which(psol[3,,6]==1))
+
+# melyik helyen melyik számok lehetnek - jobban
+jps<-t(colSums(psol)==3)
+for(i in 1:9){gn<-which(colSums(jps[1:9,])==1)
+              msudoku[which(jps[1:9,gn]==T)]<-gn
+            jps[1:9,gn]<-F
+}
+
+which(rowSums(colSums(psol)==3)[,1:9])==1
+
+
+(colSums(psol)==3)[,1:9]
+
+
+
+ms<-msudoku
+
+ut<-array(0,c(2,9,81)) # lehetséges értékek mátrixa
+for(i in 1:9){for (j in 1:9){
+  ut[1,j,which(msudoku[i,]!=0)*9-9+i]<-sum(msudoku[i,]==j) # lehetséges értékek sor szerint
+  ut[2,j,which(msudoku[,i]!=0)+(i-1)*9]<-sum(msudoku[,i]==j) # lehetséges értékek oszlop szerint
+  # if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek kis négyzetek szerint
+  #   psol[3,k,(j-1)*9+i]<-sum(!sum(msudoku[(1+3*(ceiling(i/3)-1)):(3+3*(ceiling(i/3)-1)),(1+3*(ceiling(j/3)-1)):(3+3*(ceiling(j/3)-1))]==k))
+  # 
 }}
+
+ut<-rep(0,81)
+for(i in 1:81){
+  ut[i]<-sum(msudoku[row(msudoku)[i],]==msudoku[i])+sum(msudoku[,col(msudoku)[i]]==msudoku[i])-1
+}
+sum(ut)
+
+
+  ut[1,j,which(msudoku[i,]!=0)*9-9+i]<-sum(msudoku[i,]==j) # lehetséges értékek sor szerint
+  ut[2,j,which(msudoku[,i]!=0)+(i-1)*9]<-sum(msudoku[,i]==j) # lehetséges értékek oszlop szerint
+  # if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek kis négyzetek szerint
+  #   psol[3,k,(j-1)*9+i]<-sum(!sum(msudoku[(1+3*(ceiling(i/3)-1)):(3+3*(ceiling(i/3)-1)),(1+3*(ceiling(j/3)-1)):(3+3*(ceiling(j/3)-1))]==k))
+  # 
+
+w<-t(colSums(ut))
+
+
