d83eda07bc87f3bf07424b5d621685d2b0e46aa4
huszad <39922466+huszad@users.noreply.github.com>
2018-07-11 19:52:06 +0200

    Update AI_sudoku.R

--- AI_sudoku.R
+++ AI_sudoku.R
@@ -1,5 +1,6 @@
 
 
+
 # Először le kell futtatni az egészet a "----------" vonalig
 
 easy = "9 2 5   3, 43278   ,  89    1,6   3  7 ,2 5   4 9, 1  9   6,3    19  ,   46935 ,7   8 2 4"
@@ -16,7 +17,7 @@ msudoku<<-matrix(0,9,9)
 colnames(msudoku)<<-paste("C",c(1:9),sep = "")
 rownames(msudoku)<<-paste("R",c(1:9),sep = "")
 for(i in 1:9){
-msudoku[i,]<<-rbind(as.numeric(unlist(strsplit(unlist(strsplit(easy, ","))[i],NULL))[1:9]))}                                                 
+msudoku[i,]<<-rbind(as.numeric(unlist(strsplit(unlist(strsplit(sudoku, ","))[i],NULL))[1:9]))}                                                 
 msudoku[is.na(msudoku)]<<-0
 #print(msudoku)
 }
@@ -38,12 +39,12 @@ while(sum(msudoku-msus)){
   milehet(msudoku)
   
 #elmentem új névvel, hogy össze tudjam hasonlítani
-  msus<-msudoku
+  msus<<-msudoku
   
 #beírja a számot, ha csak 1 lehetséges szám van, ami mind a három szűrő szerint szerepelhet
 for(i in 1:81){
   if(sum(colSums(psol[,,i])==3)==1){
-    msudoku[i]<-col(psol[,,80])[1,colSums(psol[,,i])==3]
+    msudoku[i]<<-col(psol[,,80])[1,colSums(psol[,,i])==3]
   }}}
   print(msudoku)
   print(Sys.time()-m)}
@@ -84,11 +85,18 @@ generate<-function(easy){
   for(i in zerohelyek){
     ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
   }
+  beolvas(easy) # hogy az eredeti kitöltetlen mátrix maradjon ezzela  névvel
 }
 
 # felcseréli a vektor két elemét
-felcsere <- function(sorrend,ezt,erre) { 
-  sorrend[c(ezt,erre)] <<- sorrend[c(erre,ezt)]} 
+felcsere <- function(regisorrend,ezt,erre) {
+  sorrend<<-regisorrend
+  #ujsor<<-sorrend
+  sorrend[c(ezt,erre)] <<- sorrend[c(erre,ezt)]
+  #sorrend<<-ujsor
+} 
+
+sorrend
 
 
 
@@ -97,6 +105,7 @@ felcsere <- function(sorrend,ezt,erre) {
 #ezelőtt futtasd generate-et
 #felcserél két elemet kisnégyzeten belül 
 valaszt<-function(){
+  #ut2<<-ut1
   eze<<-ez # hogy a végén lássam mi változott
   elem<<-matrix(1:81,9,9)
   worst<<-sample(which(ut1==max(ut1)),1) # kiválaszt a legrosszabbak közül 1-et véletlenül
@@ -104,20 +113,77 @@ valaszt<-function(){
   co<<-ceiling(col(elem)[worst]/3)
   toligr<<-(1+3*(ro-1)):(3+3*(ro-1))
   toligc<<-(1+3*(co-1)):(3+3*(co-1))
+  
+  # for(i in zerohelyek){
+  #   ut2[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
+  # }
+  
   m<<-ez[toligr,toligc]
   csere<<-ez[worst] #m[which(elem[toligr,toligc]==worst)] # ezt a számot akarom kicserélni
   sorrend<<-m[which(msudoku[toligr,toligc]==0)]# ebben a sorrendben írta be vélelenül
   ezt<<-which(sorrend==csere) # amelyiket kiválasztottam mint legrosszabb
-  #erre<<-sample(which(sorrend!=csere),1) # egy másik véletlenszerűen választott
-  erre<<-order(ut1[elem[toligr,toligc][msudoku[toligr,toligc]==0]],decreasing = T)[2]
-  felcsere(sorrend,ezt,erre)
+  minfit()
+  
+  felcsere(regisorrend,ezt,errem)
+  
   ez[toligr,toligc][msudoku[toligr,toligc]==0]<<-sorrend # beírja az új sorrendben a számokat
   
   for(i in zerohelyek){
     ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
   }
-  print(ez-eze)
+  #print(ez-eze)
 }
+
+
+sorrend
+ezt
+
+#forgatós
+valaszt<-function(){
+  eze<<-ez # hogy a végén lássam mi változott
+  elem<<-matrix(1:81,9,9)
+  worst<<-sample(which(ut1==max(ut1)),1) # kiválaszt a legrosszabbak közül 1-et véletlenül
+  ro<<-ceiling(row(elem)[worst]/3)
+  co<<-ceiling(col(elem)[worst]/3)
+  toligr<<-(1+3*(ro-1)):(3+3*(ro-1))
+  toligc<<-(1+3*(co-1)):(3+3*(co-1))
+  
+  for(i in zerohelyek){
+    ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
+  }
+  
+  m<<-ez[toligr,toligc]
+  csere<<-ez[worst] #m[which(elem[toligr,toligc]==worst)] # ezt a számot akarom kicserélni
+  sorrend<<-m[which(msudoku[toligr,toligc]==0)]# ebben a sorrendben írta be vélelenül
+  sorrend<<-sorrend[c(length(sorrend),1:(length(sorrend)-1))]
+  ez[toligr,toligc][msudoku[toligr,toligc]==0]<<-sorrend # beírja az új sorrendben a számokat
+  
+  for(i in zerohelyek){
+    ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
+  }
+}
+
+
+#legkisebb fit
+minfit<-function(){
+  fitertek<<-rep(0,(length(sorrend)))
+  tarts<<-ez
+  regisorrend<<-sorrend
+  for(j in which(regisorrend!="")[-ezt]){
+    erre<<-j
+    ez<<-tarts
+    felcsere(regisorrend,ezt,erre)
+    ez[toligr,toligc][msudoku[toligr,toligc]==0]<<-sorrend # beírja az új sorrendben a számokat
+    
+    for(i in zerohelyek){
+      ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
+    }
+    fitertek[j]<<-sum(ut1)}
+    errem<<-order(fitertek)[2]
+}
+
+
+
 
 #----------
 
@@ -136,15 +202,13 @@ jps<-t(colSums(psol)==3)
 
 
 generate(easy)
-beolvas(easy)
-valaszt()
 
 # nszam<-length(zerohelyek)
 # fitrossz<-sum(ut1)
 while(sum(ut1) > length(zerohelyek)){
-  Sys.sleep(0.3)
+  Sys.sleep(0.2)
   valaszt()
-  print(sum(ut1))
+  print(sum(ut1)-length(zerohelyek))
 }
 
 t<-system.time()
@@ -160,8 +224,6 @@ s<-ut1
 valaszt()
 ut1-s
 
-
-#innentől már nem tudom mi van, csak próbálgattam valamihez 
 
 
 length(sorrend)
@@ -251,10 +313,12 @@ ezt<-which(sorrend==csere) # amelyiket kiválasztottam mint legrosszabb
 #erre<<-sample(which(sorrend!=csere),1) # egy másik véletlenszerűen választott
 erre<-order(ut1[elem[toligr,toligc][msudoku[toligr,toligc]==0]],decreasing = T)[2]
 felcsere(sorrend,ezt,erre)
-ez[toligr,toligc][msudoku[toligr,toligc]==0]<<-sorrend # beírja az új sorrendben a számokat
+ez[toligr,toligc][msudoku[toligr,toligc]==0]<-sorrend # beírja az új sorrendben a számokat
 
 for(i in zerohelyek){
   ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
 }
+
+
 
 
