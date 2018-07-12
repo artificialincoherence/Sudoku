ca08d291abe7f35e2f800d3715889e81109115db
huszad <39922466+huszad@users.noreply.github.com>
2018-07-12 08:47:03 +0200

    Update AI_sudoku.R

--- AI_sudoku.R
+++ AI_sudoku.R
@@ -1,6 +1,7 @@
 
 
 
+
 # Először le kell futtatni az egészet a "----------" vonalig
 
 easy = "9 2 5   3, 43278   ,  89    1,6   3  7 ,2 5   4 9, 1  9   6,3    19  ,   46935 ,7   8 2 4"
@@ -29,7 +30,83 @@ for(i in 1:9){for (j in 1:9){
   psol[2,j,which(msudoku[,i]==0)+(i-1)*9]<<-sum(!sum(msudoku[,i]==j)) # lehetséges értékek oszlop szerint
   if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek kis négyzetek szerint
     psol[3,k,(j-1)*9+i]<<-sum(!sum(msudoku[(1+3*(ceiling(i/3)-1)):(3+3*(ceiling(i/3)-1)),(1+3*(ceiling(j/3)-1)):(3+3*(ceiling(j/3)-1))]==k))
-  }}}}}
+  }}}}
+jps<<-t(colSums(psol)==3)
+}
+
+
+
+
+
+jobeir<-function(){
+  
+  milehet(msudoku)# meghatározza jps -t hogy hova lehet írni mit
+  
+  mstart<<-msudoku # hogy loopnál össze tudja hasonlítani a mostanival
+  
+  #ez nézi, hogy összesen csak 1 érték lehetett-e
+ hely<<-which(rowSums(jps)==1)# melyik helyen lehet csak 1 alapból
+  #if(length(hely)>0){
+  ertek<<-which(jps[hely,])#ezeken a helyeken mit kell beírni
+  msudoku[hely]<<-ertek
+  #diag(jps[hely,ertek])<<-F
+    # subm<<-jps[hely,ertek]# visszaírja hogy ne vizsgálja újra később, amit most kivett
+    # diag(subm)<<-F
+    # jps[hely,ertek]<<-subm}#diag(jps[hely1,ertek1])<<-F ilyesmivel nem futott le (elvileg bug)
+    
+  for(i in 1:9){
+    #oszlop szerinti egyszeres eset
+    oszlop<<-(9*(i-1)+1):(9*(i-1)+9)
+    ertek1<<-which(colSums(jps[oszlop,])==10)
+    hely1<<-oszlop[row(jps[oszlop,ertek1,drop=F])[jps[oszlop,ertek1]]] # drop, hogy mátrix maradjon, és tudja a sorokat nézni
+    msudoku[hely1]<<-ertek1}# beírja az egyértelmű értéket
+  
+    #diag(jps[hely1,ertek1])<<-F #a beírt helyre visszaírja, hogy már nem lehet
+    # subm<<-jps[hely1,ertek1]# visszaírja hogy ne vizsgálja újra később, amit most kivett
+    # diag(subm)<<-F
+    # jps[hely1,ertek1]<<-subm#diag(jps[hely1,ertek1])<<-F ilyesmivel nem futott le (elvileg bug)
+    
+#sor szerinti egyszeres eset
+  for(i in 1:9){
+    sor<<-seq(i,72+i,9)
+    ertek2<<-which(colSums(jps[sor,])==1)
+    hely2<<-sor[row(jps[sor,ertek2,drop=F])[jps[sor,ertek2]]] # drop, hogy mátrix maradjon, és tudja a sorokat nézni
+    msudoku[hely2]<<-ertek2}
+    
+    # subm<<-jps[hely1,ertek1]# visszaírja hogy ne vizsgálja újra később, amit most kivett
+    # diag(subm)<<-F
+    # jps[hely1,ertek1]<<-subm
+    # #diag(jps[hely2,ertek2])<<-F
+    
+    #tömb szerinti egyszeres eset
+  for(i in 1:9){
+    tomb<<-c(seq((3*((i-1)%%3)+1+(ceiling(i/3)-1)*27),(3*((i-1)%%3)+3+(ceiling(i/3)-1)*27),1),
+            seq((9+3*((i-1)%%3)+1+(ceiling(i/3)-1)*27),(9+3*((i-1)%%3)+3+(ceiling(i/3)-1)*27),1),
+            seq(18+3*((i-1)%%3)+1+(ceiling(i/3)-1)*27,18+3*((i-1)%%3)+3+(ceiling(i/3)-1)*27,1))
+    ertek3<<-which(colSums(jps[tomb,])==1) 
+    hely3<<-tomb[row(jps[tomb,ertek3,drop=F])[jps[tomb,ertek3]]] # drop, hogy mátrix maradjon, és tudja a sorokat nézni
+    msudoku[hely3]<<-ertek3}
+    
+    # subm<<-jps[hely3,ertek3]# visszaírja hogy ne vizsgálja újra később, amit most kivett
+    # diag(subm)<<-F
+    # jps[hely3,ertek3]<<-subm
+    # #diag(jps[hely3,ertek3])<<-F
+    
+  }
+
+
+
+jobbmegold<-function(game){
+  beolvas(game)
+  m<-Sys.time()
+  mstart<<-matrix(0,9,9)
+  while(sum(msudoku-mstart)){
+    jobeir()}
+  print(msudoku)
+  print(Sys.time()-m)}
+
+
+
 
 
 megold<-function(msudoku){
@@ -319,6 +396,63 @@ for(i in zerohelyek){
   ut1[i]<<-sum(ez[row(ez)[i],]==ez[i])+sum(ez[,col(ez)[i]]==ez[i])-1
 }
 
+
+
+#################
+#backtrackinghez#
+#################
+
+
+milehet<-function(msudoku){
+  psol<<-array(0,c(3,9,81)) # lehetséges értékek mátrixa
+  for(i in 1:9){for (j in 1:9){
+    psol[1,j,which(msudoku[i,]==0)*9-9+i]<<-sum(!sum(msudoku[i,]==j)) # lehetséges értékek sor szerint
+    psol[2,j,which(msudoku[,i]==0)+(i-1)*9]<<-sum(!sum(msudoku[,i]==j)) # lehetséges értékek oszlop szerint
+    if(msudoku[(j-1)*9+i]==0){for(k in 1:9){ #lehetséges értékek kis négyzetek szerint
+      psol[3,k,(j-1)*9+i]<<-sum(!sum(msudoku[(1+3*(ceiling(i/3)-1)):(3+3*(ceiling(i/3)-1)),(1+3*(ceiling(j/3)-1)):(3+3*(ceiling(j/3)-1))]==k))
+    }}}}}
+
+
+# melyik helyen melyik számok lehetnek - jobban
+jps<-t(colSums(psol)==3)
+for(i in 1:9){
+  oszlop<-(9*(i-1)+1):(9*(i-1)+9)
+  gn<-which(colSums(jps[oszlop,])==1)
+msudoku[which(jps[1:9,gn])]<-gn
+jps[1:9,gn]<-F
+}
+
+megold<-function(msudoku){
+  m<-Sys.time()
+  mstart<<-msudoku
+  while(sum(msudoku-msus)){
+    milehet(msudoku)
+    
+    #elmentem új névvel, hogy össze tudjam hasonlítani
+    msus<<-msudoku
+    
+    #beírja a számot, ha csak 1 lehetséges szám van, ami mind a három szűrő szerint szerepelhet
+    for(i in 1:81){
+      if(sum(colSums(psol[,,i])==3)==1){
+        msudoku[i]<<-col(psol[,,80])[1,colSums(psol[,,i])==3]
+      }}}
+  print(msudoku)
+  print(Sys.time()-m)}
+
+rowsum(jps)
+
+
+
+
+milehet(msudoku)
+
+ertek<-sor[row(jps[sor,ertek2,drop=F])[jps[sor,ertek2]]]
+
+hely<-which(rowSums(jps)==1)# melyik helyen lehet csak 1 alapból
+#if(length(hely)>0){
+ertek<-row(t(jps[which(rowSums(jps)==1),]))[t(jps[which(rowSums(jps)==1),])]#ezeken a helyeken mit kell beírni
+msudoku[hely]<-ertek
+diag(jps[hely,ertek])<-F
 
 
 
